3018a5b7e1589b74953cfa4404b85513d351fa6c
bkaminski <ben@benkaminski.com>
2016-08-01 19:47:55 -0400

    Got rid of annoying "howdy" greeting

--- wp-content/themes/altarofdagon/functions.php
+++ wp-content/themes/altarofdagon/functions.php
@@ -206,5 +206,29 @@ function bandFavicon() {
  add_action( 'login_head', 'bandFavicon' );
  add_action( 'admin_head', 'bandFavicon' );
 
+//Change annoying "Howdy..." greeting
+add_action( 'admin_bar_menu', 'wp_admin_bar_my_custom_account_menu', 11 );
+
+function wp_admin_bar_my_custom_account_menu( $wp_admin_bar ) {
+  $user_id = get_current_user_id();
+  $current_user = wp_get_current_user();
+  $profile_url = get_edit_profile_url( $user_id );
+
+if ( 0 != $user_id ) {
+  $avatar = get_avatar( $user_id, 28 );
+  $howdy = sprintf( __('What it do %1$s?'), $current_user->display_name );
+  $class = empty( $avatar ) ? '' : 'with-avatar';
+
+$wp_admin_bar->add_menu( array(
+  'id' => 'my-account',
+  'parent' => 'top-secondary',
+  'title' => $howdy . $avatar,
+  'href' => $profile_url,
+  'meta' => array(
+  'class' => $class,
+),
+));
+}}
+
 //WP Helpers
 show_admin_bar(false);
