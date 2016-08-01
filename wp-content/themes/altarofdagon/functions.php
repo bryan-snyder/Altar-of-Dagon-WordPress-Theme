d72d2b623ed6b9262577c29c2e1f0648201ba057
bkaminski <ben@benkaminski.com>
2016-08-01 18:17:51 -0400

    Styled login page via functions.php
    Additionally, also in functions.php I removed some options from the
    theme customizer for areas that I will not want users to update. I only
    want them using the customizer for customizations I create. Also, added
    “retina.js” support for high resolution imagery.

--- wp-content/themes/altarofdagon/functions.php
+++ wp-content/themes/altarofdagon/functions.php
@@ -15,6 +15,7 @@ add_filter( 'clean_url', 'band_async_scripts', 11, 1 );
 function enqueue_band_scripts() {
 	wp_enqueue_script( 'bootstrap-js-cdn', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js#asyncload', array('jquery'), null, true);
 	wp_enqueue_script( 'band-js' , get_template_directory_uri() . '/js/band.js', array('jquery'), null, true);
+  wp_enqueue_script( 'retina-js' , get_template_directory_uri() . '/js/retina.min.js', array('jquery'), null, true);
 	wp_enqueue_script( 'font-awesome-cdn', '//use.fontawesome.com/27505604f5.js#asyncload', false, null, true);
 }
 add_action('wp_enqueue_scripts', 'enqueue_band_scripts');
@@ -53,7 +54,23 @@ function custom_oembed_filter($html, $url, $attr, $post_ID) {
     return $return;
 }
 
-
+function band_theme_customize_register( $wp_customize ) {
+ //=============================================================
+ // Remove header image and widgets option from theme customizer
+ //=============================================================
+ $wp_customize->remove_control('header_image');
+ $wp_customize->remove_panel('widgets');
+ $wp_customize->remove_panel('nav_menus');
+ //=============================================================
+ // Remove Colors, Background image, and Static front page
+ // option from theme customizer
+ //=============================================================
+ $wp_customize->remove_section('colors');
+ $wp_customize->remove_section('background_image');
+ $wp_customize->remove_section('static_front_page');
+ $wp_customize->remove_section('title_tagline');
+}
+add_action( 'customize_register', 'band_theme_customize_register', 20 );
 
 //begin blog page read more button
 function excerpt_read_more_link($output) {
@@ -98,6 +115,95 @@ function band_pagination($pages = '', $range = 2)
          echo "&nbsp;total pages</div>";
      }
 }
+
+//STYLE LOGIN SCREEN
+function band_login_logo() { ?>
+    <style type="text/css">
+        .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/altar_of_dagon_trans_logo.png) !important;
+            background-size: 320px !important;
+            width: 320px !important;
+            height: 180px !important
+        }
+        body {
+        	background: #070707 !important;
+          background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/stressed_linen.png) !important;
+          background-repeat: repeat
+        }
+        a:focus {
+        	box-shadow: none;
+        }
+        .login #nav a {
+          font-size: 14px !important;
+          font-weight: bold !important;
+        }
+        .login #backtoblog a {
+          font-size: 14px !important;
+          font-weight: bold !important;
+        }
+        .login form {
+        	background: #222 !important;
+          border: 2px solid #ccc;
+          border-radius: 5px;
+          box-shadow: 0px 0px 20px #3dff3d,
+          inset 0px 0px 2px #3dff3d !important;
+        }
+        .login form::before {
+        	display: block;
+        	content: "Website Administration Area";
+        	margin-top: -20px;
+        	padding-bottom: 20px;
+        	font-size: 18px;
+        	text-align: center;
+          color: white !important;
+        }
+        .login label {
+        	font-size: 16px !important;
+        	font-weight: bold;
+        	color: #3dff3d !important;
+        }
+        label[for=user_pass]:before {
+        	content: "\f023 \2002";
+        	font-family: FontAwesome;
+        	color: #3dff3d;
+        }
+        label[for=user_login]:before {
+        	content: "\f007 \2002";
+        	font-family: FontAwesome;
+        	color: #3dff3d;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'band_login_logo' );
+
+function band_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'band_login_logo_url' );
+
+function band_login_logo_url_title() {
+    return 'Altar of Dagon';
+}
+add_filter( 'login_headertitle', 'band_login_logo_url_title' );
+
+//LOAD STYLES AND SCRIPTS ON LOGIN PAGE
+function band_enqueue_style() {
+	wp_enqueue_style( 'fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css', false );
+}
+function band_enqueue_script() {
+	wp_enqueue_script( 'retina-js' , get_template_directory_uri() . '/js/retina.min.js', false, null, true);
+}
+add_action( 'login_enqueue_scripts', 'band_enqueue_style', 10 );
+add_action( 'login_enqueue_scripts', 'band_enqueue_script', 1 );
+
+
+//ADMIN SECTION FAVICON ITEMS TO <head> SECTION
+function bandFavicon() {
+ echo '<link rel="Icon" type="image/x-icon" href="http://localhost/~adminsimac/altarofdagon/wp-content/themes/altarofdagon/img/favicon-32x32.png" />
+ <link rel="Shortcut Icon" type="image/x-icon" href="http://localhost/~adminsimac/altarofdagon/wp-content/themes/altarofdagon/img/favicon-32x32.png" />';
+ }
+ add_action( 'login_head', 'bandFavicon' );
+ add_action( 'admin_head', 'bandFavicon' );
 
 //WP Helpers
 show_admin_bar(false);
